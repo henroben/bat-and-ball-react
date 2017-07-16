14fe48a3b09c27a3d46f5f092f511413fdf8e517
Ben Mercer <ben@henroben.co.uk>
2017-07-16 19:51:29 +0100

    Minor css changes

--- src/components/display_intro.js
+++ src/components/display_intro.js
@@ -5,7 +5,7 @@ export default class DisplayIntro extends Component {
         return(
             <div className="bat-and-ball-intro-screen">
                 <h1>BAT AND BALL!</h1>
-                <p>The classic through the wall game. Hit the ball up to the wall to remove the bricks and increase your score.</p>
+                <p>The classic through the wall game. Use your mouse to bounce the ball up to the wall to remove the bricks and increase your score.</p>
                 <p>You start with 3 lives, an extra life is awarded every 3 levels, but be careful, the ball gets faster as you progress further!</p>
                 <button className="btn btn-primary btn-block" onClick={this.props.handleClick}>CLICK TO PLAY</button>
             </div>
